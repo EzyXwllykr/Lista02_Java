75e3fa3c858336c070c7f5bbd5963369af52cfd9
Ezy Wallyker <nothinghere2408@gmail.com>
2024-10-19 16:30:41 -0300

    Create Q201.java

---
+++ Q201.java
@@ -0,0 +1,60 @@
+/*
+NOME: Esther Araujo Carreiro
+DISCIPLINA: Desenvolvimento de Aplicações
+TURMA: TCTG241CNTDEV
+*/
+
+public class Q201 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int opcao;
+
+        do {
+            System.out.println("Escolha a operação que deseja:");
+            System.out.println("1 - ADIÇÃO");
+            System.out.println("2 - SUBTRAÇÃO");
+            System.out.println("3 - MULTIPLICAÇÃO");
+            System.out.println("4 - DIVISÃO");
+            System.out.println("5 - SAIR");
+            System.out.println("Opção: ");
+            opcao = scanner.nextInt();
+      
+            if (opcao >= 1 && opcao <= 4) {
+                System.out.println("Digite o primeiro número: ");
+                double num1 = scanner.nextDouble();
+                System.out.println("Digite o segundo número: ");
+                double num2 = scanner.nextDouble();
+                double resultado = 0;
+
+                switch (opcao) {
+                    case 1:
+                        resultado = num1 + num2;
+                        System.out.println("Resultado: " + resultado);
+                        break;
+                    case 2:
+                        resultado = num1 - num2;
+                        System.out.println("Resultado: " + resultado);
+                        break;
+                    case 3:
+                        resultado = num1 * num2;
+                        System.out.println("Resultado: " + resultado);
+                        break;
+                    case 4:
+                        if (num2 != 0) {
+                            resultado = num1 / num2;
+                            System.out.println("Resultado: " + resultado);
+                        } else {
+                            System.out.println("Erro. Não é possível dividir por zero.");
+                        }
+                        break;
+                }
+            } else if (opcao != 5) {
+                System.out.println("Opção inválida. Tente novamente.");
+            }
+
+        } while (opcao != 5);
+
+        System.out.println("Saindo da calculadora.");
+        scanner.close();
+    }
+}
